e73dbdd590aec9d56b2450356e4bc567f440e224
Pooya Hatami <pooya@node-1w7jr9qrok5tbmfbapha9d96d.ipv6.telus.net>
2018-06-08 01:51:18 -0700

    Initial Commit

---
+++ IOS-Swift-VisionFaceDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS-Swift-VisionFaceDetection
+//
+//  Created by Pooya on 2018-06-08.
+//  Copyright © 2018 Pooya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
